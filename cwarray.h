7a714467db8e2bfe5df95a4743e7cb89aedacc11
connor <>
2024-07-27 20:02:26 +0000

    removed underscore from cwarray
    FossilOrigin-Name: 978ee45bf3a53fbbbeae1b7e01832b534baf4a19edfb031bc05d065a4b74575a

--- cwarray.h
+++ cwarray.h
@@ -8,13 +8,13 @@ typedef struct CwArray {
 	size_t capacity;
 } CwArray;
 
-CwArray* cw_array_new(size_t element_size);
-void* cw_array_push(CwArray* self);
-void* cw_array_get(CwArray* self, size_t index);
-void* cw_array_demote(CwArray* self);
+CwArray* cwarray_new(size_t element_size);
+void* cwarray_push(CwArray* self);
+void* cwarray_get(CwArray* self, size_t index);
+void* cwarray_demote(CwArray* self);
 
 #ifdef CWARRAY_IMPLEMENTATION
-CwArray* cw_array_new(size_t element_size) {
+CwArray* cwarray_new(size_t element_size) {
     CwArray* self = malloc(sizeof(CwArray));
     if (self == NULL) return NULL;
 
@@ -27,11 +27,11 @@ CwArray* cw_array_new(size_t element_size) {
     return self;
 }
 
-void* cw_array_get(CwArray* self, size_t index) {
+void* cwarray_get(CwArray* self, size_t index) {
 	return self -> ptr + (self -> element_size * index);
 }
 
-void* cw_array_push(CwArray* self) {
+void* cwarray_push(CwArray* self) {
     while (self -> size >= self -> capacity) {
         self -> capacity *= 2;
         self -> ptr = realloc(self -> ptr, self -> capacity * self -> element_size);
@@ -42,7 +42,7 @@ void* cw_array_push(CwArray* self) {
     return output;
 }
 
-void* cw_array_demote(CwArray* self) {
+void* cwarray_demote(CwArray* self) {
     void* output = self -> ptr;
     free(self);
     return output;
