516419515578b854289c77b52a6b13597cb3062f
connor <>
2024-07-27 20:00:44 +0000

    vector math and array demotion
    FossilOrigin-Name: c6d39050fc44850aef558123f1bebfdf311e046dfa3b8618c6ef4045570d5414

--- cwarray.h
+++ cwarray.h
@@ -1,7 +1,6 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-
 typedef struct CwArray {
 	void* ptr;
 	size_t element_size;
@@ -11,8 +10,10 @@ typedef struct CwArray {
 
 CwArray* cw_array_new(size_t element_size);
 void* cw_array_push(CwArray* self);
+void* cw_array_get(CwArray* self, size_t index);
+void* cw_array_demote(CwArray* self);
 
-#ifdef CW_IMPL
+#ifdef CWARRAY_IMPLEMENTATION
 CwArray* cw_array_new(size_t element_size) {
     CwArray* self = malloc(sizeof(CwArray));
     if (self == NULL) return NULL;
@@ -26,15 +27,25 @@ CwArray* cw_array_new(size_t element_size) {
     return self;
 }
 
+void* cw_array_get(CwArray* self, size_t index) {
+	return self -> ptr + (self -> element_size * index);
+}
+
 void* cw_array_push(CwArray* self) {
     while (self -> size >= self -> capacity) {
         self -> capacity *= 2;
         self -> ptr = realloc(self -> ptr, self -> capacity * self -> element_size);
     }
 
+    void* output = (void*)(self -> ptr + (self -> size * self -> element_size));
     self -> size += 1;
-    return (void*)(self -> ptr + (self -> size * self -> element_size));
+    return output;
+}
+
+void* cw_array_demote(CwArray* self) {
+    void* output = self -> ptr;
+    free(self);
+    return output;
 }
 
 #endif
-
