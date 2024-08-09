0ae37538152eade1a7800c95ade13a08a6d57e66
connor <>
2024-08-09 13:22:11 +0000

    cwstring_write_to_file
    FossilOrigin-Name: 449f8e5eed698deaf5318e2766584c535fa2d42dcca1d3184d2e5c9299fdf4ce

--- cwstring.h
+++ cwstring.h
@@ -1,5 +1,5 @@
-#ifndef CW_STRING
-#define CW_STRING
+#ifndef CWSTRING_H
+#define CWSTRING_H
 
 #include <stdint.h>
 #include <stdlib.h>
@@ -12,25 +12,27 @@ typedef struct CwString {
 	size_t capacity;
 } CwString;
 
-CwString* cw_string_new();
-CwString* cw_string_fmt_hex(uint32_t value, int digits);
-CwString* cw_string_fmt_int(uint32_t value, int digits);
-CwString* cw_string_from_file(char* path);
-CwString* cw_string_clone(CwString* self);
+CwString* cwstring_new();
+CwString* cwstring_fmt_hex(uint32_t value, int digits);
+CwString* cwstring_fmt_int(uint32_t value, int digits);
+CwString* cwstring_from_file(char* path);
+CwString* cwstring_clone(CwString* self);
 
-void cw_string_free(CwString* self);
-int cw_string_push(CwString* self, char c);
-int cw_string_push_front(CwString* self, char c);
-int cw_string_append_cstr(CwString* self, char* str);
-int cw_string_prepend_cstr(CwString* self, char* str);
-int cw_string_append_slice(CwString* self, uint8_t* src, size_t size);
+void cwstring_free(CwString* self);
+int cwstring_push(CwString* self, char c);
+int cwstring_push_front(CwString* self, char c);
+int cwstring_append_cstr(CwString* self, char* str);
+int cwstring_prepend_cstr(CwString* self, char* str);
+int cwstring_append_slice(CwString* self, uint8_t* src, size_t size);
+
+int cwstring_write_to_file(CwString* self, char* path);
 
 
 #endif
 
-#ifdef CWUTILS_IMPLEMENT_STRING
+#ifdef CWSTRING_IMPLEMENTATION
 
-CwString* cw_string_new() {
+CwString* cwstring_new() {
     CwString* self = malloc(sizeof(CwString));
     if (self == NULL) return NULL; 
 
@@ -43,65 +45,75 @@ CwString* cw_string_new() {
 }
 
 // TODO
-// CwString* cw_string_clone(CwString* self) {
+// CwString* cwstring_clone(CwString* self) {
 // 
 // }
 
-CwString* cw_string_from_file(char* path) {
+CwString* cwstring_from_file(char* path) {
     FILE* file = fopen(path, "rb");
     if (file == NULL) return NULL;
 
-	CwString* self = cw_string_new();
+	CwString* self = cwstring_new();
 	if (self == NULL) return NULL;
 
 	uint8_t buffer[128];
 	size_t bytes_read;
 	while ((bytes_read = fread(buffer, 1, 128, file)) == 128) {
-    	cw_string_append_slice(self, buffer, bytes_read);
+    	cwstring_append_slice(self, buffer, bytes_read);
 	}
 
 	if (ferror(file)) {
-    	cw_string_free(self);
+    	cwstring_free(self);
     	fclose(file);
     	return NULL;
 	}
 
-	cw_string_append_slice(self, buffer, bytes_read);
+	cwstring_append_slice(self, buffer, bytes_read);
 	return self;
 }
 
-CwString* cw_string_fmt_hex(uint32_t value, int digits) {
-    CwString* self = cw_string_new();
+int cwstring_write_to_file(CwString* self, char* path) {
+    FILE* file = fopen(path, "wb");
+    if (file == NULL) return 1;
+
+    size_t bytes_written = fwrite(self -> ptr, 1, self -> size, file);
+    if (bytes_written != self -> size) return 2;
+
+    return 0;
+}
+
+CwString* cwstring_fmt_hex(uint32_t value, int digits) {
+    CwString* self = cwstring_new();
     for(int i=0; i<digits; i++) {
         uint8_t next = (uint8_t)(value & 0x0F);
         char c = next <= 9 ? next + '0' : (next - 10) + 'A';
-        cw_string_push_front(self, c);
+        cwstring_push_front(self, c);
         value >>= 4;
     }
 
-    cw_string_push(self, '\0');
+    cwstring_push(self, '\0');
     return self;
 }
 
-CwString* cw_string_fmt_int(uint32_t value, int digits) {
-    CwString* self = cw_string_new();
+CwString* cwstring_fmt_int(uint32_t value, int digits) {
+    CwString* self = cwstring_new();
     for(int i=0; i<digits; i++) {
         uint8_t next = (uint8_t)(value % 10);
         char c = next + '0';
-        cw_string_push_front(self, c);
+        cwstring_push_front(self, c);
         value = value / 10;
     }
 
-    cw_string_push(self, '\0');
+    cwstring_push(self, '\0');
     return self;
 }
 
-void cw_string_free(CwString* self) {
+void cwstring_free(CwString* self) {
   	free(self -> ptr);
   	free(self);
 }
 
-int cw_string_push(CwString* self, char c) {
+int cwstring_push(CwString* self, char c) {
     if (self -> size >= self -> capacity) {
         do { self -> capacity <<= 1; } while (self -> size >= self -> capacity);
         self -> ptr = realloc(self -> ptr, self -> capacity);
@@ -112,7 +124,7 @@ int cw_string_push(CwString* self, char c) {
     return 0;
 }
 
-int cw_string_push_front(CwString* self, char c) {
+int cwstring_push_front(CwString* self, char c) {
     if (self -> size >= self -> capacity) {
         do { self -> capacity <<= 1; } while (self -> size >= self -> capacity);
         self -> ptr = realloc(self -> ptr, self -> capacity);
@@ -125,8 +137,8 @@ int cw_string_push_front(CwString* self, char c) {
     return 0;
 }
 
-int cw_string_append_cstr(CwString* self, char* str) {
-    while (*str != '\0') cw_string_push(self, *str++);
+int cwstring_append_cstr(CwString* self, char* str) {
+    while (*str != '\0') cwstring_push(self, *str++);
     return 0;
 }
 
