f97b82912e266433b09cb445700ef29709b33a28
connor <>
2025-02-11 03:11:11 +0000

    cwpool
    FossilOrigin-Name: 33a4513043976e1e0eb8fc6a131ba501c374bc70c5f909013a907f2127017f6e

---
+++ src/cwpool.c
@@ -0,0 +1,41 @@
+#include <cwcore.h>
+#include <stdalign.h>
+#include <string.h>
+
+typedef struct Chunk {
+	struct Chunk* next;
+} Chunk;
+
+CwPool cwpool_create(CwArena* a, size_t element_size, int size) {
+    void* pool = cwalloc(a, element_size, alignof(void*), size);
+
+	intptr_t chunk = (intptr_t)(pool);
+    for (int i=0; i<size; i++, chunk += element_size) {
+        if (i == size) {
+            ((Chunk*)(chunk)) -> next = NULL;
+        } else {
+            ((Chunk*)(chunk)) -> next = (Chunk*)(chunk + element_size); 
+        }
+    }
+
+    CwPool output;
+    output.next_free = pool;
+    output.element_size = element_size;
+    return output;
+}
+
+void* cwpool_alloc(CwPool* self) {
+    if (self == NULL || self -> next_free == NULL) return NULL;
+
+	Chunk* output = (Chunk*)(self -> next_free);
+	self -> next_free = output -> next;
+	return memset(output, 0, self -> element_size);
+	// return (void*)(output);
+}
+
+void cwpool_free(CwPool* self, void* ptr) {
+    if (self == NULL || ptr == NULL) return;
+
+    ((Chunk*)(ptr)) -> next = self -> next_free;
+    self -> next_free = ptr;
+}
